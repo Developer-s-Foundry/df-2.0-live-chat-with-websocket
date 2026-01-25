c5c87322373b3feca2d31d768141c15c163e3bab
judithukachukwu <ukachukwujudith95@gmail.com>
2026-01-25 17:58:59 +0100

    db setup

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,17 +1,12 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/ecommerce'); // ecommerce doesn't exist yet
+const mongooseConnection = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/df-2.0-live-chat-with-websocket'); // df-2.0-live-chat-with-websocket doesn't exist yet
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-
-const User = mongoose.model('User', userSchema);
-
-async function run() {
-  await User.create({ username: 'alice', password: '1234' }); // <— first insert
-  console.log('Inserted user');
-}
-
-run();
+export const connectDb = async () => {
+  try {
+    await mongooseConnection;
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
